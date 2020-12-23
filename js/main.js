5e2c4a9c30ff158d68b788169faa1725f409d06e
darley <darleycsmonte@fac.pe.senac.br>
2020-12-23 10:20:17 -0300

    segundo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function clicou() {
+    document.getElementById("darley").innerHTML = "Olá";
+
+}
+
+function clicar() {
+    window.open("https://globoesporte.globo.com/rs/futebol/times/gremio/");
+    window.location.href = "https: //globoesporte.globo.com/rs/futebol/times/gremio/";
+}
